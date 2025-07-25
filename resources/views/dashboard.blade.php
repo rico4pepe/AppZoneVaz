4efaff378228bb77dd72c629586b202af6878bf7
rico4pepe <lanre.bolaji007@gmail.com>
2025-07-25 13:33:46 +0000

    latest Update Modal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -90,7 +90,7 @@
     </div>
 </div>
 
-@if (auth()->check() && (is_null(auth()->user()->name) || (auth()->user()->team_id === 0)))
+@if (auth()->check() && empty(auth()->user()->team_id))
     <script>
    
         document.addEventListener('DOMContentLoaded', function () {
