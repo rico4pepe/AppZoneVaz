4e5dc0de5954f75b7236d23da3d750f3776736a5
rico4pepe <lanre.bolaji007@gmail.com>
2026-03-09 11:04:44 +0000

    Fix Vite 6 manifest location and add type module

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,22 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js', 'resources/js/chat.jsx', 'resources/js/poll.jsx', 'resources/js/quizz.jsx'],
+            input: [
+                'resources/js/app.js', 
+                'resources/js/chat.jsx', 
+                'resources/js/poll.jsx', 
+                'resources/js/quizz.jsx'
+            ],
             refresh: true,
         }),
         react(),
     ],
     build: {
-        outDir: 'public/build',
-        manifest: true,
-    }
+        manifest: 'manifest.json', // Move manifest to root of build dir
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
 });
